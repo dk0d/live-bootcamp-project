a96f8821b694acadc5df62b57c2c92c3e6ba4032
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2026-01-15 22:41:43 -0500

    feat(sp1-3): impl application struct

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -5,8 +5,10 @@ pub mod openapi;
 
 use std::time::Duration;
 
-use axum::Router;
-use tower_http::services::ServeDir;
+use tokio::net::TcpListener;
+
+use axum::{serve::Serve, Router};
+
 use tower_http::trace::TraceLayer;
 use tracing::Level;
 
@@ -14,30 +16,70 @@ use utoipa_scalar::{Scalar, Servable};
 
 use self::handlers::build_app_router;
 
-pub async fn build_router(config: &config::Config) -> anyhow::Result<Router> {
-    let assets_dir = ServeDir::new("assets");
+#[derive(Debug)]
+pub struct Application {
+    server: Serve<TcpListener, Router, Router>,
+    pub address: String,
+}
 
-    let (router, api) = build_app_router().split_for_parts();
+impl Application {
+    /// Build the main application router with middleware and documentation.
+    ///
+    /// This may be useful when using axum_test to map per test routers.
+    ///
+    /// Arguments:
+    /// - `config`: Application configuration. (though not used currently, extendable later)
+    ///
+    /// Returns:
+    /// - `Router`: The constructed application router.
+    pub async fn build_router(_config: &config::Config) -> anyhow::Result<Router> {
+        // let assets_dir = ServeDir::new("assets");
+        let (router, api) = build_app_router().split_for_parts();
+        let router = router.merge(Scalar::with_url("/docs", api)).layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &axum::http::Request<_>| {
+                    tracing::span!(
+                        Level::INFO,
+                            "http_request",
+                            method = %request.method(),
+                            uri = %request.uri().path(),
+                            status = tracing::field::Empty, // Status filled later
+                            latency_us = tracing::field::Empty // Latency filled later
+                    )
+                })
+                .on_response(
+                    |resp: &axum::http::Response<_>, latency: Duration, span: &tracing::Span| {
+                        span.record("status", resp.status().as_u16());
+                        span.record("latency_us", latency.as_micros());
+                    },
+                ),
+        );
+        Ok(router)
+    }
 
-    let router = router.merge(Scalar::with_url("/docs", api)).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(|request: &axum::http::Request<_>| {
-                tracing::span!(
-                    Level::INFO,
-                        "http_request",
-                        method = %request.method(),
-                        uri = %request.uri().path(),
-                        status = tracing::field::Empty, // Status filled later
-                        latency_us = tracing::field::Empty // Latency filled later
-                )
-            })
-            .on_response(
-                |resp: &axum::http::Response<_>, latency: Duration, span: &tracing::Span| {
-                    span.record("status", resp.status().as_u16());
-                    span.record("latency_us", latency.as_micros());
-                },
-            ),
-    );
+    /// Main application builder.
+    ///
+    /// Arguments:
+    /// - `config`: Application configuration.
+    ///
+    /// Returns:
+    /// - `Application`: The constructed application instance.
+    pub async fn build(config: &config::Config) -> anyhow::Result<Self> {
+        let router = Application::build_router(config).await?;
+        // Here we should use ip 0.0.0.0 so the service is listening on all the configured network interfaces.
+        // This is needed for Docker to work, which we will add later on.
+        // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
+        let address = format!("{}:{}", config.server.host, config.server.port);
+        let listener = TcpListener::bind(address.clone()).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+        Ok(Self { server, address })
+    }
 
-    Ok(router)
+    /// Run the application server.
+    pub async fn run(self) -> anyhow::Result<()> {
+        tracing::info!("Listening on {}", self.address);
+        self.server.await?;
+        Ok(())
+    }
 }
--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,5 +1,5 @@
-use libauth_service::logging;
-use libauth_service::{build_router, config};
+use libauth_service::config;
+use libauth_service::{logging, Application};
 
 use figment::{
     providers::{Env, Format, Toml},
@@ -8,25 +8,14 @@ use figment::{
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
     let config: config::Config = Figment::new()
         .merge(Toml::file("default.toml"))
-        .merge(Env::prefixed("LR__"))
+        .merge(Env::prefixed("LR__").global())
         .extract()?;
-
+    dbg!(&config);
     logging::init(&config)?;
-
-    let app = build_router(&config).await?;
-
-    // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
-    // This is needed for Docker to work, which we will add later on.
-    // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.port))
-        .await
-        .unwrap();
-
-    tracing::info!("listening on {}", listener.local_addr().unwrap());
-
-    axum::serve(listener, app).await.unwrap();
-
+    let app = Application::build(&config).await?;
+    app.run().await?;
     Ok(())
 }
