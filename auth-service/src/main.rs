b79d821bc457387832ca52292d7af6ee3c893c07
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2026-01-24 12:59:09 -0500

    fix: make sure env vars can be applied to nested config values

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -11,9 +11,11 @@ async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
     let config: config::Config = Figment::new()
         .merge(Toml::file("default.toml"))
-        .merge(Env::prefixed("LR__").global())
+        .merge(Env::prefixed("LR_").split('_'))
         .extract()?;
-    dbg!(&config);
+    Env::prefixed("LR_").iter().for_each(|(k, v)| {
+        println!("{}: {}", k, v);
+    });
     logging::init(&config)?;
     let app = Application::build(&config).await?;
     app.run().await?;
