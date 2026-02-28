58adcbcf3b717738d82f062406029221b953be60
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2026-02-28 13:10:01 -0500

    fix: migrations cleanup after some `shki` bug fixes

--- auth-service/schema/migrations/0000_init.down.sql
+++ auth-service/schema/migrations/0000_init.down.sql
@@ -1,7 +1,5 @@
 -- Migration: 0000_init (down)
--- Created at: 2026-02-25T03:45:42.393313+00:00
+-- Created at: 2026-02-28T18:07:48.725668+00:00
 -- This migration reverses the changes made by the up migration.
 
-DROP TABLE "two_factor";
---> +statement
 DROP TABLE "user";
--- auth-service/schema/migrations/0000_init.sql
+++ auth-service/schema/migrations/0000_init.sql
@@ -1,6 +1,6 @@
 -- Migration: 0000_init (up)
--- Created at: 2026-02-25T03:45:42.393210+00:00
--- To snapshot: a089836b-f849-4275-800a-ea0800f56ecc
+-- Created at: 2026-02-28T18:07:48.725522+00:00
+-- To snapshot: 3fc41e3f-d9b5-4124-933d-f4d4688f448d
 
 CREATE TABLE "user" (
   "email" TEXT PRIMARY KEY NOT NULL,
@@ -9,11 +9,3 @@ CREATE TABLE "user" (
 );
 --> +statement
 COMMENT ON TABLE "user" IS 'User accounts';
---> +statement
-CREATE TABLE "two_factor" (
-  "email" TEXT PRIMARY KEY NOT NULL,
-  "id" UUID NOT NULL DEFAULT uuidv7(),
-  "code" TEXT NOT NULL
-);
---> +statement
-COMMENT ON TABLE "two_factor" IS '2FA Codes';
